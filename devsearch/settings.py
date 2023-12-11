ba39b3d423a7fbfe658dc12ac7b6f973cf5e2c43
M-Shariq-546 <45226@gcslahore.edu.pk>
2023-12-11 16:28:51 +0500

    Messages Feature Added

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -120,6 +120,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.hotmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mshariq28022000@hotmail.com'
+EMAIL_HOST_PASSWORD = 'Shariq@123'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
--- users_info/admin.py
+++ users_info/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile , Skill
+from .models import Profile , Skill, Message
 
 
 admin.site.register(Profile)
-
+admin.site.register(Message)
 admin.site.register(Skill)
 # Register your models here.
